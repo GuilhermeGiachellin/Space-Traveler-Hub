4b5918f3cc2a0be7d42e7c710f29bfd1323a5ee7
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-09-06 16:13:47 -0300

    Basic Layout

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const App = () => {
+    <Router>
+
+    </Router>;
+};
+
+export default App;
